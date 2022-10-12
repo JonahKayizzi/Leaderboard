eb47c150d2311d5c9791960a11dc2c3c5b5b0ba4
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-10-12 21:17:24 +0300

    Create new game if there is none on localStorage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './styles.css';
 import populateScores from './modules/populateScores.js';
 import gameScores from './modules/scores.js';
+import createNewGame from './modules/createGame.js';
 
 window.onload = () => {
   const addScore = document.querySelector('.submit-button');
   const refreshButton = document.querySelector('.refresh-button');
   populateScores();
+
+  const gameID = localStorage.getItem('storedGame') || createNewGame();
+  console.log(gameID);
 
   addScore.addEventListener('click', (e) => {
     e.preventDefault();
